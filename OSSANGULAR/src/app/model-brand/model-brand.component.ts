e62f2a407642e1b075acccaa71677667ed2d80c2
Andres Coto <andres.coto3539@gmail.com>
2018-11-09 00:56:39 -0600

    server and component creo

--- OSSANGULAR/src/app/model-brand/model-brand.component.ts
+++ OSSANGULAR/src/app/model-brand/model-brand.component.ts
@@ -1,19 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductCategoryService } from '../product-category/product-category.service';
-import { User } from "../Models/user";
 import {Router} from '@angular/router';
-import { ProductCategory } from '../Models/product-category';
+import { ModelBrandService } from './model-brand.service';
+import { ModelBrand } from '../model/modelbrand.model';
 
 @Component({
   selector: 'app-model-brand',
   templateUrl: './model-brand.component.html',
-  styleUrls: ['./model-brand.component.css']
+  styleUrls: ['./model-brandy.component.css']
 })
 export class ModelBrandComponent implements OnInit {
 
-  constructor() { }
+  modelBrand: ModelBrand= new ModelBrand();
+  modelBrands: ModelBrand[]= new Array<ModelBrand>();
+
+  constructor(private router: Router, private service: ModelBrandService) { }
 
   ngOnInit() {
+    this.service.getAllModelBrands().subscribe(
+      (data: ModelBrand[])=>{
+        this.modelBrands = data;
+      }
+    )
   }
 
+  insert(){
+    alert('Usted ha ingresado correctamente '+this.modelBrand.model);
+    this.service.insertModelBrand(this.modelBrand).subscribe(
+      (data:ModelBrand) =>{
+        this.modelBrand = data;
+      }
+    )
+  }
+  update(){
+    alert('Usted ha actualizado correctamente '+this.modelBrand.model);
+    this.service.updateModelBrand(this.modelBrand).subscribe(
+      (data:ModelBrand) =>{
+        this.modelBrand = data;
+      }
+    )
+  }
+  delete(){
+    alert('Usted ha eliminado correctamente '+this.modelBrand.model);
+    this.service.deleteCategory(this.modelBrand.id).subscribe(
+      (data:ModelBrand) =>{
+        this.modelBrand = data;
+    })
+  }
 }
+
--- OSSANGULAR/src/app/model-brand/model-brand.service.ts
+++ OSSANGULAR/src/app/model-brand/model-brand.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Environment} from '../app.environment';
-import { ProductCategory } from '../Models/product-category'
-
-@Injectable({
-  providedIn: 'root'
-})
+import { ModelBrand } from '../model/modelbrand.model';
+@Injectable()
 export class ModelBrandService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private requestMapping = 'modelbrand/';
+
+  private url = Environment.apiUrl + this.requestMapping;
+
+  public insertModelBrand (modelBrand:ModelBrand){
+    console.log(modelBrand.model+ "  "+modelBrand.brand);
+    return this.http.post(this.url+'insertModelBrand/',modelBrand);
+  }
+
+  public getAllModelBrands(){
+    return this.http.get(Environment.apiUrl+'modelbrand/');
+  }
+
+  public updateModelBrand(modelBrand:ModelBrand){
+    return this.http.put(this.url+'/updateModelBrand'+ modelBrand);
+  }
+
+  public deleteCategory(modelBrandId:Number){
+    return this.http.delete(this.url+modelBrandId)
+  }
+
 }
