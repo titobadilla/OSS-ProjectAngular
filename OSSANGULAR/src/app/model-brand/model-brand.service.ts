163019e503906d17e947f482f318ea4220aef151
Andres Coto <andres.coto3539@gmail.com>
2018-11-12 23:52:14 -0600

    cambios

--- OSSANGULAR/src/app/model-brand/model-brand.service.ts
+++ OSSANGULAR/src/app/model-brand/model-brand.service.ts
@@ -1,32 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import {Environment} from '../app.environment';
 import { ModelBrand } from '../model/modelbrand.model';
 import { Observable } from 'rxjs';
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class ModelBrandService {
+
+  URLAPI=Environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
-  private requestMapping = 'modelbrand/';
-
-  private url = Environment.apiUrl + this.requestMapping;
-
-  public insertModelBrand (modelBrand:ModelBrand){
-    console.log(modelBrand.model+ "  "+modelBrand.brand);
-    return this.http.post(this.url+'insertModelBrand/',modelBrand);
+  public insertModelBrand(modelBrand:ModelBrand):Observable<ModelBrand>{
+    return this.http.post<ModelBrand>(this.URLAPI+'modelbrand/',ModelBrand );
   }
 
-  public getAllModelBrands():Observable<ModelBrand[]>{
-    return this.http.get<ModelBrand[]>(Environment.apiUrl+'modelbrand/');
+  public getAllModelBrand():Observable<ModelBrand[]>{
+    return this.http.get<ModelBrand[]>(this.URLAPI+'modelbrand');
+  }
+/*
+  public getByIdDevice(serialNumber:String):Observable<ModelBrand>{
+    return this.http.get<ModelBrand>(this.URLAPI+'modelBrand/'+serialNumber);
+  }
+*/
+  public updateModelBrand(modelBrand:ModelBrand):Observable<ModelBrand>{
+    return this.http.put<ModelBrand>(this.URLAPI+'modelBrand',modelBrand);
   }
 
-  public updateModelBrand(modelBrand:ModelBrand){
-    return this.http.put(this.url+'/updateModelBrand', modelBrand);
-  }
-
-  public deleteCategory(modelBrandId:Number){
-    return this.http.delete(this.url+modelBrandId)
+  public deleteModelBrand(serialNumber:String):Observable<ModelBrand>{
+    return this.http.delete<ModelBrand>(this.URLAPI+'modelBrand/'+serialNumber);
   }
 
 }
