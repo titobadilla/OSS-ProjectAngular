cea85c33d9ae8b5fa592deea395703b82978292e
Andres Coto <andres.coto3539@gmail.com>
2018-12-13 20:39:46 -0600

    validaciones

--- OSSANGULAR/src/app/work-order/insert-work-order/insert-work-order.component.ts
+++ OSSANGULAR/src/app/work-order/insert-work-order/insert-work-order.component.ts
@@ -7,6 +7,7 @@ import { ClientService } from 'src/app/client/client-service';
 import { Client } from 'src/app/model/client.model';
 import { Employee } from 'src/app/model/employee.model';
 import { EmployeeService } from 'src/app/employee/employee.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-insert-work-order',
@@ -16,42 +17,46 @@ import { EmployeeService } from 'src/app/employee/employee.service';
 export class InsertWorkOrderComponent implements OnInit {
 
   workOrder: WorkOrder = new WorkOrder();
-  workOrdersType:WorkOrderType[]=new Array();
-  clients:Client[]=new Array();
-  employees:Employee[]=new Array();
-  selectedEmployees:String[];
+  workOrdersType: WorkOrderType[] = new Array();
+  clients: Client[] = new Array();
+  employees: Employee[] = new Array();
+  selectedEmployees: String[];
+  public sucess = false;
 
 
-  constructor(private service: WorkOrderService,private serviceWorkOrderTypes:WorkOrderTypeService,
-    private serviceClient:ClientService,private serviceEmployee:EmployeeService) { }
+  constructor(private service: WorkOrderService, private serviceWorkOrderTypes: WorkOrderTypeService,
+    private serviceClient: ClientService, private serviceEmployee: EmployeeService) { }
 
   ngOnInit() {
     this.loading();
   }
 
-  private loading(){
-    this.serviceWorkOrderTypes.getAllWorkOrdersType().subscribe(data=>{
-      this.workOrdersType=data;
-  });
-  this.serviceClient.getAllClients().subscribe(data=>{
-    this.clients=data
-  });
+  private loading() {
+    this.serviceWorkOrderTypes.getAllWorkOrdersType().subscribe(data => {
+      this.workOrdersType = data;
+    });
+    this.serviceClient.getAllClients().subscribe(data => {
+      this.clients = data
+    });
 
-  this.serviceEmployee.getAllEmployees().subscribe(data=>{
-    this.employees=data
-  });
+    this.serviceEmployee.getAllEmployees().subscribe(data => {
+      this.employees = data
+    });
   }
 
-  public createWorkOrder(){
-    let i=0;
+  public createWorkOrder(form: NgForm) {
+    let i = 0;
 
-    for(i=0;i<this.selectedEmployees.length;i++){
-      let employee:Employee = new Employee();
-      employee.id=this.selectedEmployees[i];
+    for (i = 0; i < this.selectedEmployees.length; i++) {
+      let employee: Employee = new Employee();
+      employee.id = this.selectedEmployees[i];
       this.workOrder.employees.push(employee);
     }
     this.service.insertWorkOrder(this.workOrder).subscribe();
-      this.workOrder=new WorkOrder();
-      this.selectedEmployees=[];
+    this.workOrder = new WorkOrder();
+    this.selectedEmployees = [];
+
   }
+
 }
+
